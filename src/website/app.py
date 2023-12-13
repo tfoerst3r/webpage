46f9a7bf632fb065be0a681fb649d7607e58b5bb
Foerster, T <thomas.foerster@hzdr.de>
2023-12-13 18:37:38 +0100

    First running web app

--- src/website/app.py
+++ src/website/app.py
@@ -1,3 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Thomas!</p>"
+
+
 def main():
-    print("Hello world!")
-    print("Hello world! a second time")
+    app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    main()
